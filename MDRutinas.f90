c712698b600e0aa917b8ef6ae9ae1ec74ce5e1d5
DanoLV <daniel.la.valle.lab@gmail.com>
2023-11-12 11:54:42 -0300

    20231112

---
+++ MDRutinas.f90
@@ -0,0 +1,10 @@
+! Rutinas generales de MD
+MODULE MDRutinas
+    use, intrinsic:: iso_fortran_env, only: stdout=>output_unit, stdin=>input_unit, stderr=>error_unit
+    use ziggurat
+    IMPLICIT NONE
+
+ CONTAINS
+
+
+END MODULE MDRutinas
---
+++ globals.f90
@@ -0,0 +1,4 @@
+! Variables globales
+integer :: N
+real(kind=8), allocatable :: r(:,:), v(:,:), f(:,:)
+real(kind=8):: L
