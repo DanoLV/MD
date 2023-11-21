432bb392f90971c1b5064fa43d6e284a1d58f7c0
DanoLV <daniel.la.valle.lab@gmail.com>
2023-11-21 15:03:51 -0300

    0231121

--- md_g3.f90
+++ md_g3.f90
@@ -1,44 +1,84 @@
 ! Guia 3: Dinamica Molecular
 
-program md_g3 
+program md_g3
 
-    use globals
-    use ziggurat
-    use mdrutinas
+   use globals
+   use ziggurat
+   use mdrutinas
 
-    implicit none
-    integer :: i,N
-    real(kind=8):: L,sigma,epsilon,u,f(3),rc2
-    real(kind=8), allocatable ::r(:,:)
+   implicit none
+   logical :: es
+   integer :: seed,i,N
+   real(kind=8):: L,sigma,epsilon,u,fvec(3),rc2
+   real(kind=8), allocatable ::r(:,:),f(:,:)
 
-    sigma= 1
-    epsilon = 1
-    rc2 = 100
+   !************************************************
+![NO TOCAR] Inicializa generador de número random
 
-    ! Recibir parametros N, L
-    N = 9
-    L = 9
+   inquire(file='seed.dat',exist=es)
+   if(es) then
+      open(unit=10,file='seed.dat',status='old')
+      read(10,*) seed
+      close(10)
+      ! print *,"  * Leyendo semilla de archivo seed.dat"
+   else
+      seed = 24583490
+   end if
 
-    ! Inicializar variables
-    allocate(r(N,3))
-    ! allocate(v(N,3))
-    ! allocate(f(N,3))
+   call zigset(seed)
+![FIN NO TOCAR]
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-    ! Inicializar vectores
-    call Init_pos(N,L,r)
-    ! call Init_vel(N,L,v)
-    ! call calc_force(N,L,r,v,f)
+   sigma= 1
+   epsilon = 1
+   rc2 = (2.5*sigma)**2
 
-    ! do i = 1, N
-    !     print*, r(i,:)
-    ! end do
+   ! Recibir parametros N, L
+   N = 2
+   L = 9
 
-    ! call V_interaccion(u,N,r,sigma,epsilon)
-    ! print *, u
+   ! Inicializar variables
+   allocate(r(N,3))
+   r=0
+   ! allocate(v(N,3))
+   allocate(f(N,3))
+   f=0
 
-    f = fuerza(r(1,:), r(2,:), sigma, epsilon, rc2)
-    print *, f
+   ! Inicializar vectores
+!    call Init_pos(N,L,r)
+!    call Init_pos_rand(N,L,r)
 
-    ! Ciclos de MD
+   r(1,:)= [1,0,0]
+   r(2,:)= [2,0,0]
+
+   ! call Init_vel(N,L,v)
+   ! call calc_force(N,L,r,v,f)
+
+   do i = 1, N
+      print*, r(i,:)
+   end do
+
+   call calculos(u,f, N,r, sigma,epsilon, L,rc2)
+   print *, u
+
+!    call V_interaccion(u,N,r,sigma,epsilon)
+!    print *, u
+
+!    f = fuerza(r(1,:), r(2,:), sigma, epsilon, rc2, L)
+!    print *, f
+
+   ! Ciclos de MD
+
+
+
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+![No TOCAR]
+! Escribir la última semilla para continuar con la cadena de numeros aleatorios
+
+   open(unit=10,file='seed.dat',status='unknown')
+   seed = shr3()
+   write(10,*) seed
+   close(10)
+![FIN no Tocar]
 
 end program
--- mdrutinas.f90
+++ mdrutinas.f90
@@ -1,108 +1,160 @@
 ! Rutinas generales de MD
 MODULE mdrutinas
-    use, intrinsic:: iso_fortran_env, only: stdout=>output_unit, stdin=>input_unit, stderr=>error_unit
-    use ziggurat
-    use globals
-    IMPLICIT NONE
+   use, intrinsic:: iso_fortran_env, only: stdout=>output_unit, stdin=>input_unit, stderr=>error_unit
+   use ziggurat
+   use globals
+   IMPLICIT NONE
 
- CONTAINS
+CONTAINS
 
- SUBROUTINE Init_pos(N,L,r)
+   SUBROUTINE Init_pos(N,L,r)
 
-    integer, intent(in)  :: N
-    real (kind=8), intent(in) :: L
-    real (kind=8), allocatable :: r(:,:)
-    real(kind=8):: dl, dN
-    integer :: p, i, j, k, fin
+      integer, intent(in)  :: N
+      real (kind=8), intent(in) :: L
+      real (kind=8), allocatable :: r(:,:)
+      real(kind=8):: dl, dN
+      integer :: p, i, j, k, fin
 
-    ! Particulas que entran en un lado L
-    dN = CEILING(N**(Real(1.0/3.0))) !!!! PROBLEMA. Redondeo a int superior.
+      ! Particulas que entran en un lado L
+      dN = CEILING(N**(Real(1.0/3.0))) !!!! PROBLEMA. Redondeo a int superior.
 
-    ! Lado de un volumen de particula en la caja de lado L
-    dl = L / dN
-    fin = INT(dN )
-    p = 1
-    i=0
-        do while (i .lt. fin .and. p .le. N) 
-            j=0
-            do while (j .lt. fin .and. p .le. N) 
-                k = 0
-                do while (k .lt. fin .and. p .le. N) 
-                    r(p,:) = [REAL(i*dl)+dl/2, REAL(j*dL)+dL/2, REAL(k*dL)+dL/2]
-                    p = p + 1
-                    k = k + 1
-                end do
-                j = j + 1
+      ! Lado de un volumen de particula en la caja de lado L
+      dl = L / dN
+      fin = INT(dN )
+      p = 1
+      i=0
+      do while (i .lt. fin .and. p .le. N)
+         j=0
+         do while (j .lt. fin .and. p .le. N)
+            k = 0
+            do while (k .lt. fin .and. p .le. N)
+               r(p,:) = [REAL(i*dl)+dl/2, REAL(j*dL)+dL/2, REAL(k*dL)+dL/2]
+               p = p + 1
+               k = k + 1
             end do
-            i = i + 1
-        end do    
+            j = j + 1
+         end do
+         i = i + 1
+      end do
 
- END SUBROUTINE Init_pos
+   END SUBROUTINE Init_pos
 
- SUBROUTINE Init_pos_rand(N,L,r)
+   SUBROUTINE Init_pos_rand(N,L,r)
 
-    integer, intent(in)  :: N
-    real (kind=8), intent(in) :: L
-    real (kind=8), allocatable :: r(:,:)
-    integer :: p
-    
-    do p = 1, N
-        r(p,:) = L* [uni(), uni(), uni()]
-    end do
+      integer, intent(in)  :: N
+      real (kind=8), intent(in) :: L
+      real (kind=8), allocatable :: r(:,:)
+      integer :: p
 
- END SUBROUTINE Init_pos_rand
+      do p = 1, N
+         r(p,:) = L* [uni(), uni(), uni()]
+      end do
 
- SUBROUTINE V_interaccion(u,N,r,sigma,epsilon)
-    REAL(kind=8), intent(in):: sigma,epsilon,r(N,3)
-    INTEGER, intent(in):: N
-    REAL(kind=8), intent(out):: u
-    INTEGER :: i, j
+   END SUBROUTINE Init_pos_rand
 
-    !Inicializo potencial
-    u = 0
 
-    !Calculo todas las interacciones de pares
-    do i = 1, N-1
-        do j = i + 1, N
-            u = u + U_r(r(i,:),r(j,:),sigma,epsilon)      
-        end do
-    end do 
+   SUBROUTINE calculos(u,f, N,r, sigma, epsilon, L,rc2)
+      REAL(kind=8), intent(in):: sigma,epsilon,r(N,3),L,rc2
+      INTEGER, intent(in):: N
+      REAL(kind=8), intent(out):: u, f(N,3)
+      real(kind=8) :: rrel(3), r2,faux
+      INTEGER :: i, j
 
-    return
+      !Inicializo variables
+      u = 0
+      f = 0
 
- END SUBROUTINE V_interaccion
+      !Calculo todas las interacciones de pares
+      do i = 1, N-1
+         do j = i + 1, N
 
- REAL(kind=8) FUNCTION U_r(p1,p2,sigma,epsilon)
-    REAL(kind=8), intent(in):: p1(3),p2(3),sigma,epsilon
-    REAL(kind=8) :: r 
+            !Vector posicion relativa
+            rrel = r(i,:)-r(j,:)
 
-    !Calculo distancia entre particulas
-    r = sqrt((p1(1)-p2(1))**2+(p1(2)-p2(2))**2+(p1(3)-p2(3))**2)
+            !distancia al cuadrado
+            r2 = rrel(1)**2+rrel(2)**2+rrel(3)**2
 
-    U_r = 4.0*epsilon*(-(sigma/r)**6.0+(sigma/r)**12.0)
+            ! calculo de potenciales
+            u = u + U_r(r2**(0.5),sigma,epsilon)
 
- END FUNCTION U_r
+            !calculo de fuerzas
+            faux = fuerza(r2, sigma, epsilon, rc2, L)
+            f(i,:) = faux*rrel+f(i,:)
+            f(j,:) = -faux*rrel+f(j,:)
+            ! !Condicion periodica de contorno
+            ! if ( r2 > L**2 ) then
+            !    raux = L*r
+            !    r = r - L*raux
+            ! end if
+         end do
+      end do
 
- FUNCTION fuerza(p1, p2, sigma, epsilon, rc2)
-    REAL(kind=8), intent(in):: p1(3), p2(3), sigma, epsilon, rc2
-    REAL(kind=8) :: fuerza(3), r(3) ,r2, r2i, r6i
+      return
 
-    ! vector posicion relativa
-    r = p1-p2
-    print *, r
-    !distancia al cuadrado
-    r2 = r(1)**2+r(2)**2+r(3)**2
-    print* ,r2
-    !verifico si cumple radio de corte
-    if ( r2 < rc2 ) then
+   END SUBROUTINE calculos
 
-        !calculo fuerza
-        r2i = (sigma**2)/r2
-        r6i = r2i**3
-        fuerza = 48*r2i*r6i*epsilon*(r6i-0.5)*r
+!    SUBROUTINE V_interaccion(u,N,r,sigma,epsilon)
+!       REAL(kind=8), intent(in):: sigma,epsilon,r(N,3)
+!       INTEGER, intent(in):: N
+!       REAL(kind=8), intent(out):: u
+!       INTEGER :: i, j
 
-    end if
+!       !Inicializo potencial
+!       u = 0
 
- END FUNCTION fuerza
+!       !Calculo todas las interacciones de pares
+!       do i = 1, N-1
+!          do j = i + 1, N
+!             u = u + U_r1(r(i,:),r(j,:),sigma,epsilon)
+!          end do
+!       end do
+
+!       return
+
+!    END SUBROUTINE V_interaccion
+
+   REAL(kind=8) FUNCTION U_r(r,sigma,epsilon)
+      REAL(kind=8), intent(in):: r,sigma,epsilon
+      REAL(kind=8):: rs
+
+      rs = sigma/r
+
+      U_r = 4.0*epsilon*(-(rs)**6.0+(rs)**12.0)
+
+   END FUNCTION
+
+!    REAL(kind=8) FUNCTION U_r1(p1,p2,sigma,epsilon)
+!       REAL(kind=8), intent(in):: p1(3),p2(3),sigma,epsilon
+!       REAL(kind=8) :: r
+
+!       !Calculo distancia entre particulas
+!       r = sqrt((p1(1)-p2(1))**2+(p1(2)-p2(2))**2+(p1(3)-p2(3))**2)
+
+!       U_r1 = 4.0*epsilon*(-(sigma/r)**6.0+(sigma/r)**12.0)
+
+!    END FUNCTION U_r1
+
+   REAL(kind=8) FUNCTION fuerza(r2, sigma, epsilon, rc2, L)
+      REAL(kind=8), intent(in):: sigma, epsilon, rc2, L,r2
+      REAL(kind=8) :: r2i, r6i!,fuerza(3) , raux(3) ,r2
+
+      ! !Vector posicion relativa
+      ! r = p1-p2
+      ! !  print *, r
+
+      !   !distancia al cuadrado
+      !   r2 = r(1)**2+r(2)**2+r(3)**2
+
+      !verifico si cumple radio de corte
+      if ( r2 < rc2 ) then
+
+         !calculo fuerza
+         r2i = (sigma**2)/r2
+         r6i = r2i**3
+         fuerza = 48*r2i*r6i*epsilon*(r6i-0.5)!*r
+
+      end if
+
+   END FUNCTION fuerza
 
 END MODULE mdrutinas
